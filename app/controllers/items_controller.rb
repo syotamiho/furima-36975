186735a705ddfd951da7657a5c8ce7f896a77e24
syotamiho <mazin_bu_413@icloud.com>
2022-01-26 22:16:45 +0900

    items/newへのアクセス

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,13 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+
+  end
+
+  #private
+
+  #def item_params
+    #params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  #end
 end
