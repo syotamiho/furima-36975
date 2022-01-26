b7de74c7547855b911aab395e1c086e807dae979
syotamiho <mazin_bu_413@icloud.com>
2022-01-27 03:38:32 +0900

    出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,26 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
+  
   def index
     
   end
 
   def new
-
+    @item = Item.new
   end
 
-  #private
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-  #def item_params
-    #params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  #end
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :category_id, :situation_id, :postage_id, :city_id, :shipping_date_id, :explanation, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
   validates :title, presence: true
-  validates :category_id, presence: true
-  validates :situation_id, presence: true
-  validates :postage_id, presence: true
-  validates :city_id, presence: true
-  validates :shipping_date_id, presence: true
   validates :explanation, presence: true
   validates :price, presence: true
   validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :city_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :postage
+  belongs_to :city
+  belongs_to :shipping_date
 end
