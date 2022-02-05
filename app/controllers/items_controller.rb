d49b1efc2557b1f81f15265095d854034d5126fb
syotamiho <mazin_bu_413@icloud.com>
2022-02-05 17:12:24 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(params[:id])
+      else
+        render :edit
+      end
+  end
+  
   private
 
   def item_params
